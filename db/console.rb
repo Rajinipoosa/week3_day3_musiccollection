5f79c8e6bce3ad8b1bc4ce93743643cf45566b44
Rajini <rajinipoosa@gmail.com>
2017-03-22 22:33:26 +0000

    find method has been added to the artist and albums

--- db/console.rb
+++ db/console.rb
@@ -9,15 +9,14 @@ Artist.delete_all()
 
 artist1 = Artist.new({'name' => 'Rajini'})
 artist1.save()
-# evrthing is saved in customer object so to can call it customer.id
- album1 = Album.new({
+album1 = Album.new({
   'title' =>'crazy in love',
   'genre' => 'pop',
   'artist_id' => artist1.id
   })
 
 
- album2 = Album.new({
+album2 = Album.new({
   'title' =>'lala land',
   'genre' => 'pop',
   'artist_id' => artist1.id
--- models/album.rb
+++ models/album.rb
@@ -2,8 +2,8 @@ require('pg')
 require_relative('../db/sql_runner')
 
 class Album
-   attr_accessor :title, :genre
-    attr_reader  :id , :artist_id
+  attr_accessor :title, :genre
+  attr_reader  :id , :artist_id
   def initialize( options )
 
     @title =  options['title']
@@ -16,52 +16,58 @@ class Album
     sql = "INSERT INTO albums(title,genre,artist_id) VALUES('#{@title}',
     '#{@genre}',
     #{@artist_id}) RETURNING id;"
-    
+
     result = SqlRunner.run(sql)
-     @id = result[0]["id"].to_i
+    @id = result[0]["id"].to_i
   end
 
   def update()
-    
+
    sql = "UPDATE albums SET(
    title,
    genre,
    artist_id) = (
    '#{@title}',
    '#{@genre}',
-    #{@artist_id}) WHERE id = #{@id}"
-    
-    
-      result = SqlRunner.run(sql)
+   #{@artist_id}) WHERE id = #{@id}"
 
 
-  end
-  def self.all()
-    
+   result = SqlRunner.run(sql)
+
+
+   end
+   def self.all()
+
     sql = "SELECT * FROM albums"
-      results = SqlRunner.run(sql)
-      albums = results.map{|result| Album.new(result) }
-      return albums
+    results = SqlRunner.run(sql)
+    albums = results.map{|result| Album.new(result) }
+    return albums
 
-   
+
   end
   def self.delete_all()
-    
+
     sql = "DELETE FROM albums"
     result = SqlRunner.run(sql)
-    
-
   end
- 
+
   def artist()
 
     sql = "SELECT * FROM artists where id = #{@artist_id}"
+    result = SqlRunner.run(sql)
+    order = Artist.new(result.first)
+    return order
+  end
+  def delete()
+    sql = "DELETE FROM albums where id = #{@id}"
+
+    result = SqlRunner.run(sql)
+  end
+  def self.find(id)
+
+      sql = "SELECT * FROM albums WHERE id = #{id}"
       result = SqlRunner.run(sql)
-       order = Artist.new(result.first)
-       # puts {@artist_id}
-         # create array of orders then return them  
-        # artist_album = results[0]
-        # result = Albums.new(artist_album)
-        return order
-      end
+
+      return result.first 
+  end
 end
